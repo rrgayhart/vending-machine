3c4a0831ad3567456881137e0471b714b804f34d
rrgayhart <rrgayhart@gmail.com>
2017-03-09 15:40:51 -0800

    First test

--- tests/VendingMachine-test.js
+++ tests/VendingMachine-test.js
@@ -3,5 +3,45 @@ var expect = require('chai').expect;
 var VendingMachine = require('../lib/VendingMachine.js');
 
 describe('VendingMachine', function(){
+  var subject;
 
+  beforeEach(function(){
+    subject = new VendingMachine();
+  });
+
+  describe('.selectButton', function(){
+    context('when it is the first press', function(){
+      it('it logs the last button pressed', function(){
+        expect(subject.buttonQueue).to.equal('');
+        subject.selectButton('A');
+        expect(subject.buttonQueue).to.equal('A');
+      });
+    });
+    context('when it is the second press', function(){
+      xit('', function(){
+        
+      });
+    });
+  });
 });
+
+
+
+  //   context('when the button does not match a product', function(){
+  //     xit('' function(){
+
+  //     });
+  //   });
+  // });
+
+  // describe('.insertCredit', function(){
+  //   it('' function(){
+
+  //   });
+  // });
+
+  // describe('.returnCredit', function(){
+  //   xit('' function(){
+
+  //   });
+  // });
